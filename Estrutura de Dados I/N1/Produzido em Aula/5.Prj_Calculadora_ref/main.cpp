f14662e5eaf8d0b4b6d93db02b5ea260b081d9a1
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-02-27 08:48:10 -0300

    :boom: fix: Atualizando repositório

--- Estrutura de Dados I/N1/Produzido em Aula/5.Prj_Calculadora_ref/main.cpp
+++ Estrutura de Dados I/N1/Produzido em Aula/5.Prj_Calculadora_ref/main.cpp
@@ -1,13 +1,70 @@
 #include <iostream>
-#include <locale>
 #include <cstdlib>
+#include "calculadora.h"
 
 using namespace std;
 
-int main(int argc, char** argv) {
+int menu() {
+	cout << "Calculadora:" << endl;
+	cout << "1-Soma" << endl;
+	cout << "2-Subtracao" << endl;
+	cout << "3-Multiplicacao" << endl;
+	cout << "4-Divisao" << endl;
+	cout << "5-Especial" << endl;
+	cout << "0-Sair" << endl;
+	cout << "? ";
+	int op;
+	cin >> op;
+	return op;
+}
+
+float obter_num(int i) {
+	float num;
+	cout << "Digite o N" << i << ": ";
+	cin >> num;
+	return num;
+}
+
+int main() {
 	
-	locale::global(locale("pt_BR.utf8"));
-	cout.imbue(locale());
+	int op(0);
+	float n1, n2;
 	
+	do{
+		op = menu();
+		switch(op) {
+			case 1:
+				// Soma
+				n1 = obter_num(1);
+				n2 = obter_num(2);
+				cout << "A soma eh: " << soma(n1, n2) << endl;
+				break;
+				
+			case 2:
+				// Subtracao
+				cout << "A subtracao eh: " << subtracao(obter_num(1), obter_num(2)) << endl;
+				break;
+				
+			case 3:
+				// Multiplicacao
+				cout << "A multiplicacao eh: " << multiplicacao(obter_num(1), obter_num(2)) << endl;
+				break;
+				
+			case 4:
+				// Diviasao
+				cout << "A divisao eh: " << divisao(obter_num(1), obter_num(2)) << endl;
+				break;
+				
+			case 5:
+				// Especial
+				especial();
+				break;
+			default:
+				// Fim do programa 
+				cout << "Fim do programa!" << endl << endl;
+		}
+	} while ((op > 0) && (op < 6));
+	
+	system("pause");
 	return 0;
 }
