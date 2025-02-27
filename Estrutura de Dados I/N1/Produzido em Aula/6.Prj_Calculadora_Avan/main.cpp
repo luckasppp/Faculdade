b77915efdfe28a3370bfb14b0fab6aa6154f1c49
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-02-27 10:44:33 -0300

    :sparkles: refactor: Implementando funcionalidade do exercício proposto.

--- Estrutura de Dados I/N1/Produzido em Aula/6.Prj_Calculadora_Avan/main.cpp
+++ Estrutura de Dados I/N1/Produzido em Aula/6.Prj_Calculadora_Avan/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <iomanip> // Necessário para setprecision
 
 #include "calculadora.h"
 #include "testeNum.h"
@@ -16,6 +17,7 @@ int menu(){
 	cout << "4-Divisao" << endl;
 	cout << "5-Potencia" << endl;
 	cout << "6-Raiz" << endl;
+	cout << "7-Teste numero" << endl;
 	cout << "0-Sair do programa" << endl;
 	cout << "? ";
 	
@@ -23,6 +25,22 @@ int menu(){
 	
 	return op;
 }
+
+int subMenu() {
+	char op;
+	
+	cout << "a-Eh par?" << endl;
+	cout << "b-Eh impar?" << endl;
+	cout << "c-Eh primo?" << endl;
+	cout << "d-Voltar ao menu principal" << endl;
+	cout << "? ";
+	
+	cin >> op;
+	
+	return op;
+}
+
+
 	
 float obterNum(int i) {
 	float num;
@@ -36,39 +54,88 @@ float obterNum(int i) {
 
 int main(int argc, char** argv) {
 	
+	float n1, n2;
 	int op(0);
+	char opcao;
+	
+	cout << fixed << setprecision(2);
 	
 	do{
 		op = menu();
 		switch(op) {
 			case 1:
-				
+				n1 = obterNum(1);
+				n2 = obterNum(2);
+				cout << "A soma eh: " << soma(n1, n2) << endl;
 				break;
 			case 2:
-				
+				n1 = obterNum(1);
+				n2 = obterNum(2);
+				cout << "A subtracao eh: " << subtracao(n1, n2) << endl;
 				break;
 				
 			case 3:
-				
+				n1 = obterNum(1);
+				n2 = obterNum(2);
+				cout << "A multiplicacao eh: " << multiplicacao(n1, n2) << endl;
 				break;
 				
 			case 4:
-				
+				n1 = obterNum(1);
+				n2 = obterNum(2);
+				cout << "A divisao eh: " << divisao(n1, n2) << endl;
 				break;
 				
 			case 5:
-				
+				n1 = obterNum(1);
+				n2 = obterNum(2);
+				cout << "A potencicao eh: " << potencia(n1, n2) << endl;
 				break;
 				
 			case 6:
+				n1 = obterNum(1);
+				n2 = obterNum(2);
+				cout << "A raiz eh: " << raiz(n1, n2) << endl;
+				break;
+				
+			case 7:
+				do {
+					opcao = subMenu();
+				
+					switch(opcao) {
+						case 'a':
+							if (ehPar(obterNum(1))){
+								cout << "O numero eh par!" << endl;
+							} else {
+								cout << "O numero eh impar!" << endl;
+							}
+							break;
+							
+						case 'b':
+							if (ehImpar(obterNum(1))){
+								cout << "O numero eh impar!" << endl;
+							} else {
+								cout << "O numero eh par!" << endl;
+							}
+							break;
+							
+						case 'c':
+							if (ehPrimo(obterNum(1))){
+								cout << "O numero eh primo!" << endl;
+							} else {
+								cout << "O numero nao eh primo!" << endl;
+							}
+							break;		
+					}	
+				} while(opcao == 'a' || opcao == 'b' || opcao == 'c');
 				
 				break;
 				
-			defalt:
+			default:
 				cout << "Fim do programa" << endl;
 		}
 		
-	} while((op >= 1) && (op <= 6));
+	} while((op >= 1) && (op <= 7));
 	
 	
 	system("pause");
--- Estrutura de Dados I/N1/Produzido em Aula/6.Prj_Calculadora_Avan/testeNum.cpp
+++ Estrutura de Dados I/N1/Produzido em Aula/6.Prj_Calculadora_Avan/testeNum.cpp
@@ -1,13 +1,26 @@
 // testeNum.cpp
 
 bool ehPar(int n){
-	
+	return n%2 == 0;
 }
 
 bool ehImpar(int n){
-	
+	return n%2 != 0;
 }
 
 bool ehPrimo(int n){
+	if (n < 2) {
+		return false;
+	}
+    if (n == 2) {
+    	 return true;
+	}
+    if (n % 2 == 0) {
+    	return false;
+	}
 	
+	for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
 }
