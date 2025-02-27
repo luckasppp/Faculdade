92dc2cc3b86a5c6efbbe017ce035672a471c9137
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-02-27 06:46:32 -0300

    🎉 Commit inicial do exemplo de projeto Prj_Calculadora_Ref

---
+++ Estrutura de Dados I/N1/Produzido em Aula/5.Prj_Calculadora_Ref/calculadora.cpp
@@ -0,0 +1,25 @@
+// calculadora.cpp
+#include <iostream>
+
+using namespace std;
+
+float soma(float n1, float n2){
+	return n1 + n2;
+}
+
+float subtracao(float n1, float n2){
+	return n1 - n2;
+}
+
+float multiplicacao(float n1, float n2){
+	return n1 * n2;
+}
+
+float divisao(float n1, float n2){
+	return n1 / n2;
+}
+
+void especial() {
+	cout << "\n\n\n\n <3 <3 <3 Te amo, Ana Raquel <3 <3 <3 \n\n\n\n";	
+}
+
